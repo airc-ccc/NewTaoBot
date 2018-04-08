3ddbd70a03dc47633bd4874c43695032f81cf0d5
彭涛 <pengtaopt01@163.com>
2018-04-09 01:23:48 +0800

    修改问啊

--- libs/wx_bot.py
+++ libs/wx_bot.py
@@ -37,6 +37,12 @@ tm = TextMessage()
 fm = FormData()
 ort = Orther()
 
+"""
+    
+    [["t", "226ef818793c9a6696f1ad1a14549b55"], ["cookie2", "1e099371140b4c60bfc13f085295d7d8"], ["v", "0"], ["_tb_token_", "e3a5e69e6d8ee"], ["alimamapwag", "TW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NDsgcnY6NTkuMCkgR2Vja28vMjAxMDAxMDEgRmlyZWZveC81OS4w"], ["cookie32", "6d50a6f9ad98f2c0f0a1978a4bf9cd5a"], ["alimamapw", "QhRcX15tCANSAwIEAQ4IUwIBDFZXVQQHCwEFBFZUAQheBQZSUAY%3D"], ["cookie31", "MjE4Njc3MDYsd3BobGosZ2VuZXI0MTJAc2luYS5jb20sVEI%3D"], ["login", "W5iHLLyFOGW7aA%3D%3D"], ["cna", "2R5RE//Gs3wCAWU2iTPVVSRB"], ["isg", "BFJSCT9NXCeIIKDbfOxDPs39oBs0Y1b9yLd6ghyrfoXwL_IpBPOmDVjNmwMTRM6V"], ["apush86b7c0cd604ee7fb04c27a04565ab3a7", "%7B%22ts%22%3A1523200250579%2C%22parentId%22%3A1523200250570%7D"]]
+
+"""
+
 
 def text_reply(msg, good_url):
     print(11)
